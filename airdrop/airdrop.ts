4f8cfe4425704fe748674f3a19691c10d73934f9
icekidtech <princeklvn7568@gmail.com>
2025-01-08 17:13:42 +0100

    `Removed keypair import and creation from dev-wallet.json`

--- airdrop/airdrop.ts
+++ airdrop/airdrop.ts
@@ -2,9 +2,6 @@ import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import wallet from "./dev-wallet.json";
 import { AnchorProvider } from "@coral-xyz/anchor";
 
-// Importing keypair from wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-
 // Creating a new connection to the Solana devnet for SOL tokens
 const connection = new Connection("https://api.devnet.solana.com");
 
