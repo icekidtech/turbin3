4288487aa256f6ca2922138aa1da303fc4770718
icekidtech <princeklvn7568@gmail.com>
2025-01-08 17:12:12 +0100

    `Added dev-wallet.json and walet.json to .gitignore, updated airdrop.ts to import AnchorProvider and use dev-wallet.json, and updated enroll.ts to import necessary modules and functions.`

--- airdrop/airdrop.ts
+++ airdrop/airdrop.ts
@@ -1,5 +1,6 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import wallet from "./dev-wallet.json";
+import { AnchorProvider } from "@coral-xyz/anchor";
 
 // Importing keypair from wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -15,4 +16,34 @@ async () => {
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`);
     }
+};
+
+// Importing keypair from wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+// Creating a new connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+// Github account
+const github = Buffer.from("<icekidtech>", "utf8");
+
+// Creating an anchor project
+const provider = new AnchorProvider(connection, new Wallet(keypair), {commitment: "confirmed"});
+
+// Creating the program
+const program : Program<Turbin3Prereq> = new Program(IDL, provider);
+
+// Creating the PDA for my enrollment account
+const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
+const [enrollment_key, _bump] = PublicKey.findProgramAddress(enrollment_seeds, program.programId);
+
+// Executing my enrollment tracsaction
+async () => {
+    const tshash = await program.methods.complete(github).accounts({
+        signer: keypair.publicKey,
+    })
+    .signers([keypair]).rpc();
+    console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+} catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
 };
